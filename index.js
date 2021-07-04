597a9c9d560c51c3a727f0a67ebff8194297b87c
Gordon Martin <gordonhughmartin@gmail.com>
2021-07-04 17:31:09 +0100

    Change parameters to expect stream parameter (for more flexibility.)

--- index.js
+++ index.js
@@ -2,69 +2,63 @@ var Promise = require('bluebird');
 var pull = require('pull-stream');
 var unique = require("array-unique").immutable;
 
-module.exports = function AboutOOO(server, opts) {
-  if (!server.links) throw new Error('ssb-ooo-about requires you to have the ssb-links plugin installed')
+module.exports = function AboutOOO(getAboutStream) {
+
+  // A non-live stream of all 'about 'messages in descending order
+  if (getAboutStream == null) {
+    throw new Error ("getAboutStream parameter required")
+  }
 
   return {
     async: {
-      getLatestMsgIds: (id, cb) => getLatestMsgIds(server, id, cb),
-      getLatestNameMsgId: (id, cb) => getLatestNameMsgId(server, id, cb),
-      getLatestDescriptionMsgId: (id, cb) => getLatestIds(server, id, cb),
-      getLatestPictureMsgsId: (id, cb) => getLatestIds(server, id, cb)
+      getLatestMsgIds: (id, cb) => getLatestMsgIds(id, cb),
+      getLatestNameMsgId: (id, cb) => getLatestNameMsgId(id, cb),
+      getLatestDescriptionMsgId: (id, cb) => getLatestIds(id, cb),
+      getLatestPictureMsgsId: (id, cb) => getLatestIds(id, cb)
     }
   }
-}
 
-function getLatestMsgIds(server, id, cb) {
-  var latestName = Promise.promisify(getLatestNameMsgId);
-  var latestDescription = Promise.promisify(getLatestDescriptionMsgId);
-  var latestPicture = Promise.promisify(getLatestPictureMsgsId);
+  function getLatestMsgIds(id, cb) {
+    var latestName = Promise.promisify(getLatestNameMsgId);
+    var latestDescription = Promise.promisify(getLatestDescriptionMsgId);
+    var latestPicture = Promise.promisify(getLatestPictureMsgsId);
 
-  var idsPromise = Promise.all(
-    [
-      latestName(server, id),
-      latestDescription(server, id),
-      latestPicture(server, id)
-    ]
-  );
+    var idsPromise = Promise.all(
+      [
+        latestName(id),
+        latestDescription(id),
+        latestPicture(id)
+      ]
+    );
 
-  return idsPromise.then(ids => unique(ids).filter(id => id != null)).asCallback(cb);
-}
+    return idsPromise.then(ids => unique(ids).filter(id => id != null)).asCallback(cb);
+  }
 
-function getLatestNameMsgId(server, id, cb) {
-  var aboutStream = getAboutStream(server, id);
+  function getLatestNameMsgId(id, cb) {
+    var aboutStream = getAboutStream(id);
 
-  pull(aboutStream, findKeyInStream(msg => msg.value.content.name, cb));
-}
+    pull(aboutStream, findKeyInStream(msg => msg.value.content.name, cb));
+  }
 
-function getLatestDescriptionMsgId(server, id, cb) {
-  var aboutStream = getAboutStream(server, id);
+  function getLatestDescriptionMsgId(id, cb) {
+    var aboutStream = getAboutStream(id);
 
-  pull(aboutStream, findKeyInStream(msg => msg.value.content.description, cb));
-}
+    pull(aboutStream, findKeyInStream(msg => msg.value.content.description, cb));
+  }
 
-function getLatestPictureMsgsId(server, id, cb) {
-  var aboutStream = getAboutStream(server, id);
+  function getLatestPictureMsgsId(id, cb) {
+    var aboutStream = getAboutStream(id);
 
-  pull(aboutStream, findKeyInStream(msg => msg.value.content.image, cb));
-}
+    pull(aboutStream, findKeyInStream(msg => msg.value.content.image, cb));
+  }
 
-function findKeyInStream(findFn, cb) {
-  return pull.find(findFn, (err, result) => {
-    if (err) {
-      cb(err);
-    } else {
-      cb(err, result ? result.key : null);
-    }
-  })
-}
-
-function getAboutStream(server, id) {
-  return server.links({
-    dest: id,
-    rel: "about",
-    reverse: true,
-    values: true,
-    source: id
-  });
+  function findKeyInStream(findFn, cb) {
+    return pull.find(findFn, (err, result) => {
+      if (err) {
+        cb(err);
+      } else {
+        cb(err, result ? result.key : null);
+      }
+    })
+  }
 }
